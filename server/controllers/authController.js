969b67653190fda78416b6bffd3f094711cf3a00
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-03-23 02:33:29 +0530

    google oauth -user data saved on db

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,17 @@
+import User from "../models/userModel.js";
 
-export const googleOAuth = async (req,res) => {
+export const googleOAuth = async (req, res) => {
 	console.log(req.body);
+	try {
+		const user = new User({
+			name: req.body.name,
+			email: req.body.email
+		});
+		console.log(user);
+		const success = await user.save();
+		res.status(201).json({ user });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 };
-
